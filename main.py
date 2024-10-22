10a1c9ba86d552283eeb198e95d084585fac5de5
gcqi1976 <109086322+gcqi1976@users.noreply.github.com>
2024-10-22 15:18:32 +0800

    Update main.py

--- main.py
+++ main.py
@@ -517,10 +517,7 @@ beijing_time = utc_time + timedelta(hours=8)
 # 格式化为所需的格式
 formatted_time = beijing_time.strftime("%Y%m%d %H:%M:%S")
 
-about_video1="https://gitee.com/kabigo/tv/raw/master/assets/about1080p.mp4"
-about_video2="https://gitlab.com/p2v5/wangtv/-/raw/main/about1080p.mp4"
-version=formatted_time+","+about_video1
-about="关于本源,"+about_video2
+
 # 瘦身版
 all_lines_simple =  ["更新时间,#genre#"] +[version] +[about] + ['\n'] +\
              ["💓专享源🅰️,#genre#"] + read_txt_to_array('主频道/♪专享源①.txt') + ['\n'] + \
