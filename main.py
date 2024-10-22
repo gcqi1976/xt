f5872ef6e17503b04e171b435c91ccbd42f8b975
gcqi1976 <109086322+gcqi1976@users.noreply.github.com>
2024-10-22 15:55:18 +0800

    Update main.py

--- main.py
+++ main.py
@@ -517,8 +517,8 @@ beijing_time = utc_time + timedelta(hours=8)
 # 格式化为所需的格式
 formatted_time = beijing_time.strftime("%Y%m%d %H:%M:%S")
 
-about_video1=""
-about_video2=""
+about_video1="https://raw.kkgithub.com/gcqi1976/2022/main/d.mp4"
+about_video2="https://raw.kkgithub.com/gcqi1976/2022/main/d.mp4"
 version=formatted_time+","+about_video1
 about="关于本源,"+about_video2
 # 瘦身版
